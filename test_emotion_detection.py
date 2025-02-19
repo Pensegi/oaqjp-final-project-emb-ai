2c4aac3da6f6a7d26cccb2894d017fa5c33aee82
Dennis Rudin <dennisrudin@Mac.lan>
2025-02-19 23:32:01 +0100

    step 4

--- test_emotion_detection.py
+++ test_emotion_detection.py
@@ -2,14 +2,24 @@ import EmotionDetection as ed
 
 #{[Statement]: [Dominant Emotion]}
 test_cases = {
-    "I am glad this happened":	"joy",
-    "I am really mad about this": "anger",
-    "I feel disgusted just hearing about this": "disgust",
-    "I am so sad about this": "sadness",
-    "I am really afraid that this will happen": "fear",
+    "joy": "I am glad this happened",
+    "anger": "I am really mad about this",
+    "disgust": "I feel disgusted just hearing about this",
+    "sadness": "I am so sad about this",
+    "fear": "I am really afraid that this will happen",
 }
 
-def test_assert_dominant_emotion():
-    for text, emotion in test_cases.items():
-        print(f"text: {text}: emotion:: {emotion}")
-        assert ed.emotion_detector(text)["dominant_emotion"] == emotion
+def test_assert_dominant_emotion_joy():
+    assert ed.emotion_detector(test_cases["joy"])["dominant_emotion"] == "joy"
+
+def test_assert_dominant_emotion_anger():
+    assert ed.emotion_detector(test_cases["anger"])["dominant_emotion"] == "anger"
+
+def test_assert_dominant_emotion_disgust():
+    assert ed.emotion_detector(test_cases["disgust"])["dominant_emotion"] == "disgust"
+
+def test_assert_dominant_emotion_sadness():
+    assert ed.emotion_detector(test_cases["sadness"])["dominant_emotion"] == "sadness"
+
+def test_assert_dominant_emotion_fear():
+    assert ed.emotion_detector(test_cases["fear"])["dominant_emotion"] == "fear"
