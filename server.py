7feafcdf7723a7d8807cb7ce28fb2ac4fc7f8920
Dennis Rudin <dennisrudin@Denniss-MacBook-Air.local>
2025-02-20 11:11:25 +0100

    step 6

--- server.py
+++ server.py
@@ -1,6 +1,18 @@
-from flask import render_template
+from flask import render_template, Flask, request
+from EmotionDetection import emotion_detector
 
-@app.route('/hello/')
-@app.route('/hello/<name>')
-def hello(name=None):
-    return render_template('hello.html', person=name)
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html', system_response="")
+
+@app.route('/emotionDetector')
+def emotionDetector():
+    textToAnalyze = request.args.get("textToAnalyze")
+    print(textToAnalyze)
+    response = emotion_detector(textToAnalyze)
+    return render_template('index.html', response=response)
+
+if __name__ == "__main__":
+    app.run(debug=True)
